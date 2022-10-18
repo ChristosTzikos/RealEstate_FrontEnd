45c51d2771407147432ac6331546510adfb6c274
ChristosTzikos <xristostzikos19981@gmail.com>
2022-10-18 16:20:25 +0300

    RealEstate_FrontEnd

--- app/src/main/java/com/example/real_estate/IntroActivity.kt
+++ app/src/main/java/com/example/real_estate/IntroActivity.kt
@@ -4,20 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.constraintlayout.widget.ConstraintLayout
 
 
-class IntoActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
-
-        val findBtn = findViewById<Button>(R.id.button)
-        findBtn.setOnClickListener {
-            val intent = Intent (this,MainActivity::class.java)
-            startActivity(intent)
-
-        }
-
     }
 }
--- app/src/main/java/com/example/real_estate/MainActivity.kt
+++ app/src/main/java/com/example/real_estate/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.real_estate
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity {
-
-    class IntoActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
