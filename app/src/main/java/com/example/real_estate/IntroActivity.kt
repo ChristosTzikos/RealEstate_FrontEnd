158187bb4f81336f2cce5b6dc6a192b553e61938
ChristosTzikos <xristostzikos19981@gmail.com>
2022-10-19 15:40:49 +0300

    RealEstate_FrontEnd

--- app/src/main/java/com/example/real_estate/IntroActivity.kt
+++ app/src/main/java/com/example/real_estate/IntroActivity.kt
@@ -4,11 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.example.real_estate.databinding.ActivityIntroBinding
 
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_intro)
+
+
+
+
     }
+
 }
