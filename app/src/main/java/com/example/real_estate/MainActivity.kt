1b12da168a470c21605028e2f114eb665c5b99c3
ChristosTzikos <xristostzikos19981@gmail.com>
2022-10-19 22:53:48 +0300

    RealEstate_FrontEnd

--- app/src/main/java/com/example/real_estate/MainActivity.kt
+++ app/src/main/java/com/example/real_estate/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.real_estate
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.AbsSavedState
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.main_fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.main_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
