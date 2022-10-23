3d258f554a1bd557aeff2dfa26d7644b969cc287
ChristosTzikos <xristostzikos19981@gmail.com>
2022-10-24 01:01:10 +0300

    RealEstate_FrontEnd(RECYCLER TEST)

--- app/src/main/java/com/example/real_estate/RecyclerAdapter.kt
+++ app/src/main/java/com/example/real_estate/RecyclerAdapter.kt
@@ -1,4 +1,66 @@
 package com.example.real_estate
 
-class RecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    // edw logika tha pairnoyme apo back end
+
+    private var titles = arrayOf("Large Apartment" ,"Large Apartment" ,"Large Apartment" )
+
+    private var details = arrayOf("Panorama Thessaloniki","Panorama Thessaloniki","Panorama Thessaloniki")
+
+    private var prices = arrayOf("900$" ,"900$","900$")
+
+    private var squares = arrayOf("120 sq.m.", "120 sq.m." , "120 sq.m.")
+
+    private val images = intArrayOf(R.drawable.apartment , R.drawable.apartment , R.drawable.apartment  )
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout,parent,false)
+        return ViewHolder(v)
+
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+
+    }
+
+
+    override fun onBindViewHolder(holder:RecyclerAdapter.ViewHolder , position: Int) {
+        holder.itemTitle.text = titles[position]
+        holder.itemDetail.text = details[position]
+        holder.itemPrice.text = prices[position]
+        holder.itemSquare.text = squares[position]
+        holder.itemImage.setImageResource(images[position])
+    }
+
+    inner class ViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail:TextView // topothesia
+        var itemPrice: TextView
+        var itemSquare: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.card_image)
+            itemTitle = itemView.findViewById(R.id.card_title)
+            itemDetail = itemView.findViewById(R.id.card_location)
+            itemPrice = itemView.findViewById(R.id.card_price)
+            itemSquare = itemView.findViewById(R.id.sqr_ft)
+            }
+
+    }
+
 }
+
+
--- app/src/main/java/com/example/real_estate/SecondFragment.kt
+++ app/src/main/java/com/example/real_estate/SecondFragment.kt
@@ -5,20 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+
+import androidx.recyclerview.widget.RecyclerView
+
 
 
 class SecondFragment : Fragment() {
 
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
 
     override fun onCreateView(
+
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+
+
+        recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        recyclerView.adapter = adapter
+
+
+
+
+
+
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
+
+
     }
+
+
 
 
 }
